0e6df7e33317d0ced33e4fd755942e5223e7470a
VictorHugoMA <victor.alves1@ufu.br>
2021-05-28 12:07:56 -0300

    add prog

--- Codigos/prog.c
+++ Codigos/prog.c
@@ -2,12 +2,12 @@
 #include<string.h>
 #include<stdlib.h>
 
-    int open_txt();
+    int open_txt(char *argv);
 
     int main(int argc, char *argv[]){
 
         if(strcmp(argv[1], "-open")==0){
-            open_txt();
+            open_txt(argv[2]);
         }
         else{
             printf("Comando nao encontrado\n");
@@ -16,8 +16,9 @@
         return 0;
     }
 
-    int open_txt(){
-        printf("Abrir texto\n");
+    int open_txt(char *argv){
+        printf("Abrir\n");
+        printf("%s\n", argv);
     }
 
     
